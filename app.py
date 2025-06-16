846fb576441085e68798b0bcd0c0540086841b67
Ameen Khan <ameenkhan8k@gmail.com>
2025-06-16 12:04:49 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from youtube_transcript_api import YouTubeTranscriptApi
+
+app = Flask(__name__)
+
+
+@app.route('/transcript', methods=['GET'])
+def get_transcript():
+    video_id = request.args.get('id')
+    try:
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        return jsonify(transcript)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
